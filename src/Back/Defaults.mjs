d68b026e15b1d7cfea5176459b87f8be36e1553c
Alex Gusev <alex@flancer64.com>
2021-08-18 17:38:02 +0300

    WF-477 Change license from GPL to ASF

--- src/Back/Defaults.mjs
+++ src/Back/Defaults.mjs
@@ -3,8 +3,6 @@
  */
 export default class TeqFw_I18n_Back_Defaults {
 
-    DESC_NODE = 'i18n'; // node in 'teqfw.json' descriptors
-
     DIR_BACK = 'back'; // subfolder to store i18n resources for backend scripts
     DIR_FRONT = 'front'; // subfolder to store i18n resources for frontend scripts
     DIR_I18N = 'i18n'; // root folder in plugin to store i18n resources (./i18n/front/lang.json)
